dc3d5c31137b8a7ee109a201d8560ea830143044
Prabhat Malhan <prabhat.malhan7@gmail.com>
2023-11-10 10:13:03 +0530

    empty string error handled

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -15,20 +15,22 @@ async function get_solution(eq) {
 
 async function display_result() {
   sol = await get_solution(document.getElementById("floatingInput").value);
-  if (output_showing == false) {
-    oc = document.createElement("div");
-    oc.setAttribute("id", "output_container");
-    pd = document.createElement("div");
-    pd.setAttribute("id", "output_div");
-    out = document.createElement("input");
-    out.setAttribute("type", "text");
-    out.setAttribute("class", "form-control");
-    out.setAttribute("id", "Output");
-    out.setAttribute("disabled", "");
-    out.value = sol;
-    pd.appendChild(out);
-    oc.appendChild(pd);
-    document.getElementById("body").appendChild(oc);
-    output_showing = true;
-  } else document.getElementById("Output").value = sol;
+  if (sol != "") {
+    if (output_showing == false) {
+      oc = document.createElement("div");
+      oc.setAttribute("id", "output_container");
+      pd = document.createElement("div");
+      pd.setAttribute("id", "output_div");
+      out = document.createElement("input");
+      out.setAttribute("type", "text");
+      out.setAttribute("class", "form-control");
+      out.setAttribute("id", "Output");
+      out.setAttribute("disabled", "");
+      out.value = sol;
+      pd.appendChild(out);
+      oc.appendChild(pd);
+      document.getElementById("body").appendChild(oc);
+      output_showing = true;
+    } else document.getElementById("Output").value = sol;
+  }
 }
